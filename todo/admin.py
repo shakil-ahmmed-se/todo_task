d5bc36274a529580ce61c1f4347ea2bd392dc766
shakil-ahmmed-se <mdshakilahmmed8906@gmail.com>
2024-09-15 16:33:07 +0600

    add some to admins

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Todo,Profile,PriorityChoices
 # Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+
+class PriorityAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    list_display=['name','slug']
+
+admin.site.register(Todo, TodoAdmin)
+admin.site.register(Profile)
+admin.site.register(PriorityChoices)
